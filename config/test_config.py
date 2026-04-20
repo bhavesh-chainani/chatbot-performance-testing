02c0ba08ba61ecd4cc32e08b1a6149010e2b87e3
Bhavesh Hiranand Chainani <bhavesh.hiranand.chainani@pwc.com>
2026-04-20 11:32:57 +0800

    Updated code.

--- config/test_config.py
+++ config/test_config.py
@@ -109,4 +109,4 @@ WAIT_TIME_MAX = _get_config('user_behavior.wait_time.max', 5.0, 'WAIT_TIME_MAX')
 REPORTS_DIR = _get_config('reporting.reports_dir', 'reports', 'REPORTS_DIR')
 
 # ---- Request Timeouts ----
-CHAT_TIMEOUT_SECONDS = _get_config('chatbot.request_timeout_seconds', 240, 'CHAT_TIMEOUT_SECONDS')
+CHAT_TIMEOUT_SECONDS = _get_config('chatbot.request_timeout_seconds', 300, 'CHAT_TIMEOUT_SECONDS')
